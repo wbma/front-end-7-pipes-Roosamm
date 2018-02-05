26dca96f558fcfc2befd60c699b3d2e630fd5a4c
Roosa Makela <roosamm@metropolia.fi>
2018-02-05 13:11:53 +0200

    not finished

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {MediaService} from '../services/media.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  constructor(public mediaService: MediaService, private router: Router) {
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('token') !== null) {
+      this.mediaService.getUserData().subscribe(response => {
+        console.log('Welcome ' + response['full_name']);
+        this.router.navigate(['front']);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+        this.router.navigate(['login']);
+      });
+    }
+  }
+
+}
